fa3e7128c5332d74754717c4ab9cee7bb6878cb6
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-01 17:26:49 +0600

    fixed the textsizing issue in overview

--- src/Pages/Home/Overview/Overview.jsx
+++ src/Pages/Home/Overview/Overview.jsx
@@ -9,19 +9,21 @@ export const Overview = () => {
     heading={"Overview"}
     subheading={"Making Adventure Tours, Activities Affordable"}
     ></Title>
-     <div className='flex flex-col md:flex-row  items-center  container mx-auto w-full my-16'>
+     <div className='flex
+      flex-col-reverse md:flex-row  items-center  container mx-auto w-full my-16'>
         
-        <div className="w-1/2 space-y-12">
-       <h1 className='text-5xl font-bold '> Making Adventure tours, activities affordable.</h1>
-<p className='text-3xl'>Find an adventure, create a hobby that is 
+        <div className="px-3 py-4 md:space-y-12">
+       <h1 className='text-2xl md:text-5xl font-bold mb-2'> 
+       Discover Unique Outdoor Adventures for Everyone</h1>
+<p className='text-base sm:text-lg md:text-3xl'>Find an adventure, create a hobby that is 
     related to nature in its vert
      pristiene shape for your goodness. Tempor incididunt ut labore. Et dolore 
     magna aliqua. Quitts ipsum suspendisse ultrices gravida.</p>
         </div>
      
-     <div className="w-1/2 h-[400px]">
-     {/* <iframe className='w-full h-full rounded-lg '
-      
+     <div className="md:w-1/2 h-[400px]">
+     <iframe className='w-full h-full rounded-lg '
+      width={'400px'}
         height="315"
         src="https://www.youtube.com/embed/Zcx247sfxPM?si=ieloQWAz_VVu1en1"
         title="YouTube video player"
@@ -29,10 +31,10 @@ export const Overview = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
-      ></iframe> */}
-      <ReactPlayer
+      ></iframe>
+      {/* <ReactPlayer
       url={'https://youtu.be/5s8fs_j2xlY?si=IkCsRw8bO9bhitg8'}
-      ></ReactPlayer>
+      ></ReactPlayer> */}
      </div>
     </div>
    </div>
